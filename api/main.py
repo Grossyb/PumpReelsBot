ab11c515e5138c156cd1ca83925ea7aa60677849
Grossyb <grossyb@yahoo.com>
2025-04-04 22:51:51 -0400

    bug fix

--- api/main.py
+++ api/main.py
@@ -34,7 +34,7 @@ IMAGE, PROMPT_TEMPLATES, PROMPT = range(3)
 
 prompt_templates = {
     "TO THE MOON": "It is in the cockpit of a spacecraft, pressing buttons and gazing out at the Moon through the window",
-    "WEN LAMBO": "the penguin is driving a red lamborghini down a road in South Beach, sunny, luxurious background",
+    "WEN LAMBO": "it is driving a red lamborghini down a road in South Beach, sunny, luxurious background",
     "WAGMI": "It is dressed in a black suit with a tie, sitting in private jet seat next to window. Champagne on table, luxurious interior",
 }
 
